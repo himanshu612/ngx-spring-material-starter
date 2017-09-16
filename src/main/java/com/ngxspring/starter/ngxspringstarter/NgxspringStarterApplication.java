6e3cf7a74bc946f92232b354710247bbbca3cf71
jan25 <abhilashgnan@gmail.com>
2017-09-16 11:46:35 +0800

    spring boot init

---
+++ src/main/java/com/ngxspring/starter/ngxspringstarter/NgxspringStarterApplication.java
@@ -0,0 +1,12 @@
+package com.ngxspring.starter.ngxspringstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgxspringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgxspringStarterApplication.class, args);
+	}
+}
