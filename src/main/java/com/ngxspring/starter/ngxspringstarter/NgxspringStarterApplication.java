82eb58c07ff473f455cbb2cd36cdf8a8cdf1c1b1
jan25 <abhilashgnan@gmail.com>
2017-09-16 12:47:01 +0800

    set up spring web fixes #2

--- src/main/java/com/ngxspring/starter/ngxspringstarter/NgxspringStarterApplication.java
+++ src/main/java/com/ngxspring/starter/ngxspringstarter/NgxspringStarterApplication.java
@@ -2,8 +2,10 @@ package com.ngxspring.starter.ngxspringstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ngxspring.starter.controller")
 public class NgxspringStarterApplication {
 
 	public static void main(String[] args) {
