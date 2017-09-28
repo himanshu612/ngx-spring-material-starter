cb847f5a033dfe091cc32fe861fc8020079d0125
Abhilash Reddy <uppunuru_g@worksap.co.jp>
2017-09-28 11:44:45 +0800

    mongo config added to main-app class, with service and db models

--- src/main/java/com/ngxspring/starter/controller/HelloController.java
+++ src/main/java/com/ngxspring/starter/controller/HelloController.java
@@ -1,15 +1,39 @@
 package com.ngxspring.starter.controller;
 
+import com.ngxspring.starter.models.User;
+import com.ngxspring.starter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("")
     public String hello() {
         return "Hello, World!";
     }
+
+    @RequestMapping("/{name}")
+    public User getByName(@PathVariable String name) {
+        return userService.getUser(name);
+    }
+
+    @RequestMapping("/add")
+    public User addUser(@RequestParam String name,
+                        @RequestParam(required = false) String email,
+                        @RequestParam(required = true) String userName,
+                        @RequestParam(required = true) String password) {
+        return userService.addUser(name, email, userName, password);
+    }
+
 }
--- src/main/java/com/ngxspring/starter/ngxspringstarter/NgxspringStarterApplication.java
+++ src/main/java/com/ngxspring/starter/ngxspringstarter/NgxspringStarterApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@ComponentScan("com.ngxspring.starter.controller")
+@ComponentScan(basePackages = {"com.ngxspring.starter.controller", "com.ngxspring.starter.service"})
+@Import({
+		MongoConfig.class
+})
+@EnableMongoRepositories(basePackages = {"com.ngxspring.starter.models"})
 public class NgxspringStarterApplication {
 
 	public static void main(String[] args) {
